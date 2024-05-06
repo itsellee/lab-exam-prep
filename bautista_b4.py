9083007e5e42f8ddd5e07eff2f367d776aa93232
itsellee <mbbautista9@up.edu.ph>
2024-05-06 11:37:31 +0800

    B4

--- bautista_b4.py
+++ bautista_b4.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 Table = pd.read_csv("ExamTable.csv")
-uniqspecies = (Table['Scientific Name'].unique())
+uniquespecies = (Table['Scientific Name'].unique())
 
 NewTable = pd.DataFrame()
-NewTable['Scientific Name']= uniqspecies
+NewTable['Scientific Name']= uniquespecies
 
 AveSize = []
-for i in range(len(uniqspecies)): 
-    AveSize.append((Table.loc[Table['Scientific Name']== uniqspecies[i], ['Size Est (cm)']].mean().values[0]))
+for i in range(len(uniquespecies)): 
+    AveSize.append((Table.loc[Table['Scientific Name']== uniquespecies[i], ['Size Est (cm)']].mean().values[0]))
 NewTable['Average Size Est (cm)'] = AveSize
 NewTable.to_csv('b4_output1.csv')
