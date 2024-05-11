771ec6a4cce3784b88ce0d32fae145efa9a378a3
itsellee <mbbautista9@up.edu.ph>
2024-05-11 16:21:46 +0800

    try

--- bautista_b1.py
+++ bautista_b1.py
@@ -6,3 +6,8 @@ NewTable = pd.DataFrame()
 NewTable = pd.concat([NewTable, Table.loc[Table['Interval']=="30-0"]], ignore_index=True)
 NewTable.to_csv('b1_output1.csv')
 
+
+# Read table 1 and then export it to create a table 1 copy 
+# Iupytmportant command data.loc that will locate the id's; will also be used for filtering and have a copy of the data frame
+# Pandas match; pandas api reference 
+# More on .csv manipulation (ex. transposition of the first 5 columns, or the first 2-th columns lang)
